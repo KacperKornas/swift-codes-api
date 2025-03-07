7f134271b513102fab99890fe2ea7e27f69e2579
KacperKornas <kornaskacper1@gmail.com>
2025-03-07 11:02:13 +0100

    Update SwiftCode.java

--- src/main/java/com/kacper/swiftapi/entity/SwiftCode.java
+++ src/main/java/com/kacper/swiftapi/entity/SwiftCode.java
@@ -1,12 +1,8 @@
 package com.kacper.swiftapi.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class SwiftCode {
 
     @Id
@@ -22,6 +18,51 @@ public class SwiftCode {
     @Column(nullable = false)
     private String country;
 
-    @Column
-    private String city;
+    // Konstruktor domyślny
+    public SwiftCode() {
+    }
+
+    // Konstruktor z parametrami
+    public SwiftCode(Long id, String code, String bankName, String country) {
+        this.id = id;
+        this.code = code;
+        this.bankName = bankName;
+        this.country = country;
+    }
+
+    // Getter i Setter dla 'id'
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter i Setter dla 'code'
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    // Getter i Setter dla 'bankName'
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    // Getter i Setter dla 'country'
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
