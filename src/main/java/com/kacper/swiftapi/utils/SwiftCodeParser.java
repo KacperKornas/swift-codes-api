0763d6ec634ad43f2c040227120bd11eb497c42a
KacperKornas <kornaskacper1@gmail.com>
2025-03-09 10:00:25 +0100

    Update SwiftCodeParser.java

--- src/main/java/com/kacper/swiftapi/utils/SwiftCodeParser.java
+++ src/main/java/com/kacper/swiftapi/utils/SwiftCodeParser.java
@@ -2,12 +2,7 @@ package com.kacper.swiftapi.utils;
 
 import com.kacper.swiftapi.entity.SwiftCode;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.*;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +11,7 @@ import java.util.List;
 
 @Component
 public class SwiftCodeParser {
+
     public List<SwiftCode> parseExcelFile(InputStream is) throws IOException, InvalidFormatException {
         List<SwiftCode> swiftCodes = new ArrayList<>();
         Workbook workbook = WorkbookFactory.create(is);
@@ -26,21 +22,29 @@ public class SwiftCodeParser {
                 firstRow = false;
                 continue;
             }
+
             SwiftCode swiftCodeObj = new SwiftCode();
-            String code = getCellValueAsString(row.getCell(0)).trim().toUpperCase();
-            String bankName = getCellValueAsString(row.getCell(1)).trim();
-            String country = getCellValueAsString(row.getCell(2)).trim().toUpperCase();
-            String address = "";
-            if (row.getPhysicalNumberOfCells() > 3) {
-                address = getCellValueAsString(row.getCell(3)).trim();
-            }
-            swiftCodeObj.setSwiftCode(code);
+
+            String countryISO2 = getCellValueAsString(row.getCell(0)).trim().toUpperCase();
+
+            String swiftCode = getCellValueAsString(row.getCell(1)).trim().toUpperCase();
+
+            String codeType = getCellValueAsString(row.getCell(2)).trim().toUpperCase();
+            boolean isHeadquarter = codeType.contains("HQ") || codeType.equals("HEADQUARTERS");
+
+            String bankName = getCellValueAsString(row.getCell(3)).trim();
+
+            String address = getCellValueAsString(row.getCell(4)).trim();
+
+            String countryName = getCellValueAsString(row.getCell(6)).trim().toUpperCase();
+
+            swiftCodeObj.setCountryISO2(countryISO2);
+            swiftCodeObj.setCountryName(countryName);
+            swiftCodeObj.setSwiftCode(swiftCode);
             swiftCodeObj.setBankName(bankName);
             swiftCodeObj.setAddress(address);
-            swiftCodeObj.setCountryISO2(country);
-            swiftCodeObj.setCountryName(country);
-            boolean isHeadquarter = code.endsWith("XXX");
             swiftCodeObj.setHeadquarter(isHeadquarter);
+
             swiftCodes.add(swiftCodeObj);
         }
         workbook.close();
@@ -49,13 +53,18 @@ public class SwiftCodeParser {
 
     private String getCellValueAsString(Cell cell) {
         if (cell == null) return "";
-        if (cell.getCellType() == CellType.STRING)
+        if (cell.getCellType() == CellType.STRING) {
             return cell.getStringCellValue();
-        else if (cell.getCellType() == CellType.NUMERIC)
-            return String.valueOf((int) cell.getNumericCellValue());
-        else if (cell.getCellType() == CellType.BOOLEAN)
+        } else if (cell.getCellType() == CellType.NUMERIC) {
+            if (DateUtil.isCellDateFormatted(cell)) {
+                return cell.getDateCellValue().toString();
+            } else {
+                return String.valueOf((int) cell.getNumericCellValue());
+            }
+        } else if (cell.getCellType() == CellType.BOOLEAN) {
             return String.valueOf(cell.getBooleanCellValue());
-        else
+        } else {
             return "";
+        }
     }
 }
